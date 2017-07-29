ae632a01dfb0d098599a996082f1ce415c5a39ca
Zachary T. Bagley <zbagley@datastandards.limited>
2017-07-29 15:42:19 -0600

    Added lodash use example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  list = [];
+
+  ngOnInit() {
+  	_([1,2,3,4,0]).each (i => this.list.push(i));
+  }
+
 }
